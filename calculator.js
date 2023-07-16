4e06f8fcd621a9fd9f7352d0f693a5e0a949b5a7
Muhammad Abubakar Farooq <abubakarfarooq52@gmail.com>
2023-07-16 20:49:55 +0500

    Add files via upload
    Arithmetic operator is used with variables to concatenates variables with strings. This can be done without Arithmetic operator but it's good practice to use Arithmetic operator (+) for concatenation.

--- calculator.js
+++ calculator.js
@@ -1,14 +1,14 @@
 var num1 = 5;
 var num2 = 23;
 // Addition
-console.log("\n", "Addition of", num1, "&", num2, "gives", num1 + num2, "\n");
+console.log("\n", "Addition of", +num1, "&", +num2, "gives", +(num1 + num2), "\n");
 // Subtraction
-console.log(" Subtraction of", num1, "&", num2, "gives", num1 - num2, "\n");
+console.log(" Subtraction of", +num1, "&", +num2, "gives", (+num1 - num2), "\n");
 // Multiplication 
-console.log(" Multiplication of", num1, "&", num2, "gives", num1 * num2, "\n");
+console.log(" Multiplication of", +num1, "&", +num2, "gives", +(num1 * num2), "\n");
 // Division
-console.log(" Division of", num2, "&", num1, "gives", num2 / num1, "\n");
+console.log(" Division of", +num2, "&", +num1, "gives", +(num2 / num1), "\n");
 //Exponentiation
-console.log(" Exponentiation of", num1, "&", num2, "gives", Math.pow(num1, num2), "\n");
+console.log(" Exponentiation of", +num1, "&", +num2, "gives", +(Math.pow(num1, num2)), "\n");
 //Modulus
-console.log(" Modulus of", num2, "&", num1, "gives", num2 % num1, "\n");
+console.log(" Modulus of", num2, "&", num1, "gives", +(num2 % num1), "\n");
--- calculator.ts
+++ calculator.ts
@@ -3,26 +3,26 @@ let num2=23;
 
 // Addition
 
-console.log("\n",`Addition of`, num1, `&`, num2, `gives`,num1+num2,"\n");
+console.log("\n",`Addition of`, +num1, `&`, +num2, `gives`, +(num1+num2), "\n");
 
 // Subtraction
 
-console.log(` Subtraction of`, num1, `&`, num2, `gives`, num1-num2, "\n");
+console.log(` Subtraction of`, +num1, `&`, +num2, `gives`, (+num1-num2), "\n");
 
 // Multiplication 
 
-console.log(` Multiplication of`, num1, `&`, num2, `gives`, num1*num2, "\n");
+console.log(` Multiplication of`, +num1, `&`, +num2, `gives`, +(num1*num2), "\n");
 
 // Division
 
-console.log(` Division of`, num2, `&`, num1, `gives`, num2/num1, "\n");
+console.log(` Division of`, +num2, `&`, +num1, `gives`, +(num2/num1), "\n");
 
 //Exponentiation
 
-console.log(` Exponentiation of`, num1, `&`, num2, `gives`, num1**num2, "\n");
+console.log(` Exponentiation of`, +num1, `&`, +num2, `gives`, +(num1**num2), "\n");
 
 //Modulus
 
-console.log(` Modulus of`, num2, `&`, num1, `gives`, num2%num1,"\n");
+console.log(` Modulus of`, num2, `&`, num1, `gives`, +(num2%num1),"\n");
 
 
